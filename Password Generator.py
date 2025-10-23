779799716401ff34cb6da630d5d04b2658271ab3
Cnoc-4045 <finlayjj68@gmail.com>
2025-10-23 14:12:59 +0100

    Improve logic

--- Password Generator.py
+++ Password Generator.py
@@ -2,6 +2,7 @@
 import random
 import math
 
+alphabetUpper=[""]*26
 alphabet=[""]*26
 number=[""]*10
 symbol=["!","@","#","$","%","^","&","*","(",")"]
@@ -12,15 +13,27 @@ for i in range(26):
 for i in range(10):
     number[i]=str(i)
 
-combined=number+alphabet+symbol
+for i in range(26):
+    alphabetUpper[i]=chr(65+i)
 
-def generate_password(combined):
+combined=number+alphabet+symbol+alphabetUpper
+
+length=int(input("Enter the length of password: "))
+
+password_chars = [
+    random.choice(alphabetUpper),
+    random.choice(alphabet),
+    random.choice(number),
+    random.choice(symbol)
+]
+
+def generate_password(combined,length):
     password=""
-    length=int(input("Enter the length of password: "))
-    for i in range(length):
+    password+="".join(password_chars)
+    for i in range(length-4):
         password+=random.choice(combined)
     return password
 
-password=generate_password(combined)
+password=generate_password(combined,length)
 
 print(f"Generated Password: {password}")
