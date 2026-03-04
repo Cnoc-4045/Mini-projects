ed32bc54bf9ff38a883914a41c5b6a1a191ce85a
Cnoc-4045 <finlayjj68@gmail.com>
2026-03-04 18:55:59 +0000

    Incomplet fixes

--- randomEncryption.py
+++ randomEncryption.py
@@ -1,36 +1,47 @@
 #randomEncryption
 
 import random
+import encryption
 
 def generateRandomMapping():
-    # Original alphabet: lowercase + uppercase
     alphabet = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()")
     shuffled = alphabet.copy()
     random.shuffle(shuffled)
-    # Mapping: original → substitute
     mapping = dict(zip(alphabet, shuffled))
     return mapping
 
 def encrypt(text, mapping):
-    # Replace each character with its substitute
     return ''.join(mapping.get(c, c) for c in text)
 
 def decrypt(text, mapping):
-    # Reverse mapping: substitute → original
     reverse_mapping = {v: k for k, v in mapping.items()}
     return ''.join(reverse_mapping.get(c, c) for c in text)
 
+#Main Program
+
 previousPassword=str(input("Do you have a password to encrypt? (y/n): "))
 
+if(previousPassword.lower()=="n"):
+    import passwordGenerator 
+    passwordGenerator
+    password=passwordGenerator.generate_password(combined,length)
+
 if(previousPassword.lower()=="y"):
-    password=str(input("Enter the password to encrypt: "))
 
-    mapping = generateRandomMapping()
+    password=input("Enter the password you want to encrypt: ")
 
-    encryption = encrypt(password, mapping)
+    cypherChoice=str(input("Do you want to encrypt the password using Ceaser Cypher? (y/n): "))
 
-    print(f"Encrypted Password: {encryption}")
+    if(cypherChoice.lower()=="y"):
+        shift=int(input("Enter the shift number for Ceaser Cypher: "))
+        encryptionPassword=encryption.caesarCypher(password,shift)
+        print(f"Encrypted Password: {encryptionPassword}")
 
-    # For demonstration, decrypting back to original
-    decrypted = decrypt(encryption, mapping)
-    print(f"Decrypted Password: {decrypted}")
+    randomChoice=str(input("Do you want to encrypt the password using Random Encryption? (y/n): "))
+
+    if (randomChoice.lower()=="y"):
+        mapping = generateRandomMapping()
+
+        encryptionRandomPassword=encrypt(password, mapping)
+
+        print(f"Encrypted Password using Random Encryption: {encryptionRandomPassword}")
